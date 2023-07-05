add reshape operation and fix reduce_sum when gradient with axis!

CGs.py
@@ -103,7 +103,12 @@ class Minimum(Operation):
         super().__init__([A,B], name)
     def compute(self,A_val,B_val):
         return np.minimum(A_val,B_val)
-
+class Reshape(Operation):
+    def __init__(self, A,new_shape, name='') -> None:
+        super().__init__([A], name)
+        self.newshape=new_shape
+    def compute(self,A_val):
+        return np.reshape(A_val,self.newshape)
 
 def cgsfunc(func):
     def wrapper(*args,**kvagrs):
@@ -271,6 +276,10 @@ def maximum(A,B,name=''):
 def minimum(A,B,name=''):
     minimum_obj=Minimum(A,B,name+'_ops')
     return Variable(minimum_obj.compute(A.value,B.value),name,minimum_obj)
+@cgsfunc
+def reshape(A,newshape,name=''):
+    reshaper=Reshape(A,newshape,name+'_ops')
+    return Variable(reshaper.compute(A.value),name,reshaper)
 
 def traverse_postorder(var_obj):
     nodes_postorder=[]

CGs_grad.py
@@ -63,7 +63,7 @@ def _reduce_sum_gradient(op,grad):
     A=op.input_nodes[0]
     output_shape=np.array(A.shape)
     output_shape[op.axis]=1
-    grad=np.reshape(grad,output_shape)
+    grad=reshape(grad,newshape=output_shape)
     return [grad]
 
 @RegGrad('Expandim')
@@ -94,6 +94,9 @@ def _minimum_gradient(op,grad):
     A,B=op.input_nodes
     where_a_lt_b=A<B
     return grad*where_a_lt_b.value,grad*~where_a_lt_b.value
+@RegGrad('Reshape')
+def _reshape_gradient(op,grad):
+    return [reshape(grad,newshape=op.input_nodes[0].shape)]
 
 def gradients(target_var):
     grad_dict={target_var:Variable(np.ones_like(target_var.value))}