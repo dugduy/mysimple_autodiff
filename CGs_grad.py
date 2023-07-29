Fix reduce_sum and reduce_mean gradient on multi axis calculating!!!

CGs_grad.py
@@ -62,7 +62,7 @@ def _log_gradient(node,grad):
 def _reduce_sum_gradient(node,grad):
     A=node.ops.input_nodes[0]
     output_shape=np.array(A.shape)
-    output_shape[node.ops.axis]=1
+    output_shape[list(node.ops.axis)]=1
     grad=reshape(grad,newshape=output_shape)
     return [grad]
 
@@ -70,12 +70,12 @@ def _reduce_sum_gradient(node,grad):
 def _reduce_sum_gradient(node,grad):
     A=node.ops.input_nodes[0]
     output_shape=np.array(A.shape)
-    output_shape[node.ops.axis]=1
+    output_shape[list(node.ops.axis)]=1
     grad=reshape(grad,newshape=output_shape)
     if node.ops.axis is None:
         devide_for=node.ops.input_nodes[0].size
     else:
-        devide_for=np.array(A.shape)[node.ops.axis]
+        devide_for=np.prod(np.array(A.shape)[list(node.ops.axis)])
     return [grad/devide_for]
 
 @RegGrad('Expandim')