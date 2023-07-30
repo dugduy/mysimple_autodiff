Added unfold method and its gradient function!!!!!!! fix getitem!!

CGs.py
@@ -166,6 +166,25 @@ class Reduce_Max(Operation):
     def compute(self,A_val):
         return np.max(A_val,self.axis,keepdims=self.keep_dims)
 
+class Unfold(Operation):
+    def __init__(self, A,axis,size,step, name='') -> None:
+        super().__init__([A], name)
+        self.axis=axis
+        self.kernel_size=size
+        self.step=step
+    def compute(self,A_val):
+        output_shape=np.array(A_val.shape+(self.kernel_size,))
+        output_shape[self.axis]=(output_shape[self.axis]-self.kernel_size)//self.step+1
+        transposing=np.arange(len(output_shape))
+        transposing[self.axis],transposing[0]=transposing[0],transposing[self.axis]
+
+        final_output=np.zeros(output_shape).transpose(transposing)
+        x=A_val.transpose(transposing[:-1])
+        transpose_x=tuple(range(1,x.ndim))+(0,)
+        for i in range(final_output.shape[0]):
+            final_output[i]=x[i*self.step:i*self.step+self.kernel_size].transpose(transpose_x)
+        return final_output.transpose(transposing)
+
 def cgsfunc(func):
     def wrapper(*args,**kvagrs):
         input_nodes=[]
@@ -378,7 +397,10 @@ def reduce_max(A,axis=None,keep_dims=False,name=''):
 @cgsfunc
 def reduce_min(A,axis=None,keep_dims=False,name=''):
     return -reduce_max(-A,axis=axis,keep_dims=keep_dims,name=name)
-
+@cgsfunc
+def unfold(A,axis,kernel_size,step,name=''):
+    unfolding=Unfold(A,axis,kernel_size,step,name+'_ops')
+    return Variable(unfolding.compute(A.value),name,unfolding)
 
 @cgsfunc
 def exp(x,name=''):

CGs_grad.py
@@ -111,7 +111,7 @@ def _reshape_gradient(node,grad):
     return [reshape(grad,newshape=node.ops.input_nodes[0].shape)]
 @RegGrad('Zeros_padding')
 def _zeros_pad_gradient(node,grad):
-    return grad.__getitem__(node.ops.container)
+    return [grad.__getitem__(node.ops.container)]
 @RegGrad('GetItem')
 def _getitem_gradient(node,grad):
     return [zeros_pad(grad,zeros_shape=node.ops.input_nodes[0].shape,container=node.ops.items)]
@@ -135,6 +135,18 @@ def _max_gradient(node,grad):
     output_shape=np.array(A.shape)
     output_shape[node.ops.axis]=1
     return [reshape(grad,newshape=output_shape)*where_eq_max/reduce_sum(where_eq_max,axis=node.ops.axis,keep_dims=True)]
+@RegGrad('Unfold')
+def _unfold_gradient(node,grad):
+    A=node.ops.input_nodes[0]
+    transposing=np.arange(grad.ndim)
+    transposing[0],transposing[node.ops.axis]=transposing[node.ops.axis],transposing[0]
+    final=transpose(Variable(np.zeros_like(A.value)),new_dim_index=transposing[:-1])
+    grad=transpose(grad,new_dim_index=transposing)
+    transpose_x=(-1,)+np.arange(A.ndim)
+    for i in range(grad.shape[0]):
+        final+=zeros_pad(transpose(grad[i],new_dim_index=transpose_x),zeros_shape=final.shape,container=slice(i*node.ops.step,i*node.ops.step+node.ops.kernel_size))
+    return [transpose(final,new_dim_index=transposing[:-1])]
+
 def gradients(target_var):
     grad_dict={target_var:Variable(np.ones_like(target_var.value))}
     steps=traverse_postorder(target_var)